e1e3f6a7267996584df11b1d9d6041c04ef10daf
Cze Wen Goh <czewengoh@Hotmail.com>
2016-02-12 18:59:01 -0800

    Heuristic tree

--- src/TreeNodeHeuristicVersion.java
+++ src/TreeNodeHeuristicVersion.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
-public class TreeNode {
 
-	ArrayList<TreeNode> children;
+public class TreeNodeHeuristicVersion {
+	ArrayList<TreeNodeHeuristicVersion> children;
 	int node_count = 0;
 	int a = -1;
 	int b = -1;
@@ -13,67 +13,65 @@ public class TreeNode {
 	int g = -1;
 	int h = -1;
 	int[] variables = new int[8];
-	ArrayList<TreeNode> leaves;
-	ArrayList<TreeNode> solutions;
+	ArrayList<TreeNodeHeuristicVersion> leaves;
+	ArrayList<TreeNodeHeuristicVersion> solutions;
 	String print = "";
 	int last_assigned_index;
 	int constraint_check_fail = 0;
-	TreeNode parent;
+	TreeNodeHeuristicVersion parent;
 	
-
-	public TreeNode() {
-		children = new ArrayList<TreeNode>();
+	public TreeNodeHeuristicVersion() {
+		children = new ArrayList<TreeNodeHeuristicVersion>();
 		for (int i = 0; i < 8; i++) {
 			variables[i] = -1;
 		}
 	}
 
-	public void addChild(TreeNode child) {
+	public void addChild(TreeNodeHeuristicVersion child) {
 		children.add(child);
 	}
 
-	public void deleteChild(TreeNode child) {
+	public void deleteChild(TreeNodeHeuristicVersion child) {
 		children.remove(child);
 	}
 
 	public void setA(int num) {
-		variables[0] = num;
-		//System.out.println("abcd: " + variables[0]);
+		variables[6] = num;
 		a = num;
 	}
 
 	public void setB(int num) {
-		variables[1] = num;
+		variables[5] = num;
 		b = num;
 	}
 
 	public void setC(int num) {
-		variables[2] = num;
+		variables[1] = num;
 		c = num;
 	}
 
 	public void setD(int num) {
-		variables[3] = num;
+		variables[2] = num;
 		d = num;
 	}
 
 	public void setE(int num) {
-		variables[4] = num;
+		variables[7] = num;
 		e = num;
 	}
 
 	public void setF(int num) {
-		variables[5] = num;
+		variables[4] = num;
 		f = num;
 	}
 
 	public void setG(int num) {
-		variables[6] = num;
+		variables[3] = num;
 		g = num;
 	}
 
 	public void setH(int num) {
-		variables[7] = num;
+		variables[0] = num;
 		h = num;
 	}
 
@@ -85,104 +83,87 @@ public class TreeNode {
 		return true;
 	}
 
-	public boolean constraintCheckA() {
-		if(a>0){
-			if (g > 0 && a <= g) {
+	public boolean constraintCheckH(){
+		if(h>0){
+			if(e>0 && e== h-2)
 				return false;
-			}
-			if (h > 0 && a > h) {
+			if(f>0 && f==h)
 				return false;
-			}
-		}
-		return true;
-	}
-
-	public boolean constraintCheckB() {
-		if(b>0){
-			if (f > 0 && Math.abs(f - b) != 1)
+			if(d>0 && d==h)
+				return false;
+			if(c>0 && Math.abs(h-c)%2!=0)
+				return false;
+			if(g>0 && g >= h)
+				return false;
+			if(a>0 && a>h)
 				return false;
 		}
 		return true;
 	}
-
-	public boolean constraintCheckC() {
+	
+	public boolean constraintCheckC(){
 		if(c>0){
-			if (g > 0 && Math.abs(g - c) != 1)
+			if(f>0 && f==c)
 				return false;
-			if (h > 0 && Math.abs(h - c) % 2 != 0)
+			if(e>0 && e==c)
 				return false;
-			if (d > 0 && d == c)
+			if(d>0 && d==c)
 				return false;
-			if (e > 0 && e == c)
-				return false;
-			if (f > 0 && c == f)
-				return false;
-		}
+			if(g>0 && Math.abs(g-c)!=1)
+				return false;	
+		}	
 		return true;
 	}
-
-	public boolean constraintCheckD() {
+	
+	public boolean constraintCheckD(){
 		if(d>0){
-			if (h > 0 && h == d)
+			if(f>0 && f==d)
 				return false;
-			if (g > 0 && d <= g)
+			if(e>0 && e>=(d-1))
 				return false;
-			if (e > 0 && e >= (d - 1))
-				return false;
-			if (f > 0 && d == f)
-				return false;
-		}
-		return true;	
-	}
-
-	public boolean constraintCheckE() {
-		if(e>0){
-			if (h > 0 && e == (h - 2))
-				return false;
-			if (f > 0 && Math.abs(e - f) % 2 == 0)
-				return false;
+			if(g>0 && d<=g)
+				return false;		
 		}
 		return true;
 	}
-
-	public boolean constraintCheckF() {
-		if(f>0){
-			if (g > 0 && g == f)
-				return false;
-			if (h > 0 && h == f)
-				return false;
-		}
-		return true;
-	}
-
-	public boolean constraintCheckG() {
+	
+	public boolean constraintCheckG(){
 		if(g>0){
-			if (h > 0 && g >= h)
+			if(f>0 && f==g)
+				return false;
+			if(a>0 && a<=g)
+				return false;
+		}
+		return true;
+	}
+	
+	public boolean constraintCheckF(){
+		if(f>0){
+			if(e>0 && Math.abs(e-f)%2==0)
+				return false;
+			if(b>0 && Math.abs(f-b)!=1)
 				return false;
 		}
 		return true;
 	}
 
 	public boolean consistencyCheck() {
-		if (constraintCheckA() == false)
-			return false;
-		if (constraintCheckB() == false)
+		if (constraintCheckH() == false)
 			return false;
 		if (constraintCheckC() == false)
 			return false;
 		if (constraintCheckD() == false)
 			return false;
-		if (constraintCheckE() == false)
+		if (constraintCheckG() == false)
 			return false;
 		if (constraintCheckF() == false)
 			return false;
-		if (constraintCheckG() == false)
-			return false;
 		return true;
 	}
+	
 
-	public TreeNode copy() {
-		TreeNode copy = new TreeNode();
+	public TreeNodeHeuristicVersion copy() {
+		TreeNodeHeuristicVersion copy = new TreeNodeHeuristicVersion();
 		for (int i = 0; i < 8; i++) {
 			copy.variables[i] = variables[i];
 		}
@@ -204,34 +185,34 @@ public class TreeNode {
 		}
 		switch (index) {
 		case 0:
-			setA(num);
+			setH(num);
 			break;
 		case 1:
-			setB(num);
-			break;
-		case 2:
 			setC(num);
 			break;
-		case 3:
+		case 2:
 			setD(num);
 			break;
-		case 4:
-			setE(num);
-			break;
-		case 5:
-			setF(num);
-			break;
-		case 6:
+		case 3:
 			setG(num);
 			break;
+		case 4:
+			setF(num);
+			break;
+		case 5:
+			setB(num);
+			break;
+		case 6:
+			setA(num);
+			break;
 		case 7:
-			setH(num);
+			setE(num);
 			break;
 		}
 		last_assigned_index = index;
 	}
 	
-	public void setParent(TreeNode node){
+	public void setParent(TreeNodeHeuristicVersion node){
 		this.parent = node;
 	}
 	
@@ -244,28 +225,28 @@ public class TreeNode {
 			if(variables[i]>0){
 				switch(i){
 				case 0:
-					print += "A: "+a+" ";
+					print += "H: "+h+" ";
 					break;
 				case 1:
-					print += "B: "+b+" ";
-					break;
-				case 2:
 					print += "C: "+c+" ";
 					break;
-				case 3:
+				case 2:
 					print += "D: "+d+" ";
 					break;
-				case 4:
-					print += "E: "+e+" ";
-					break;
-				case 5:
-					print += "F: "+f+" ";
-					break;
-				case 6:
+				case 3:
 					print += "G: "+g+" ";
 					break;
+				case 4:
+					print += "F: "+f+" ";
+					break;
+				case 5:
+					print += "B: "+b+" ";
+					break;
+				case 6:
+					print += "A: "+a+" ";
+					break;
 				case 7:
-					print += "H: "+h+" ";
+					print += "E: "+e+" ";
 					break;
 				}
 			}
@@ -279,9 +260,28 @@ public class TreeNode {
 		System.out.println(print);
 	}
 	
+	public boolean solutionCheck(TreeNode node){
+		if(a != node.a)
+			return false;
+		if(b != node.b)
+			return false;
+		if(c != node.c)
+			return false;
+		if(d != node.d)
+			return false;
+		if(e != node.e)
+			return false;
+		if(f != node.f)
+			return false;
+		if(g != node.g)
+			return false;
+		if(h != node.h)
+			return false;
+		return true;
+	}
+	
 	public String toString(){
 		return "A: "+a+" B: " +b+" C: " +c+" D: " +d+ " E: " +e+ " F: "+f+ " G: "+g+" H: "+h;
 	}
 	
-
 }
--- src/main.java
+++ src/main.java
@@ -1,34 +1,34 @@
 import java.math.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class main {
 
 	public static void main(String[] args){
 		TreeNode tree = constructNonHeuristicTree();
-		System.out.println(tree.node_count);
-		System.out.println(tree.leaves.size());
-		for(TreeNode node : tree.leaves){
-			if(node.consistencyCheck()){
-				System.out.println();
-				node.printElements();
-			}
+		System.out.println("Solutions: ");
+		for(TreeNode solution : tree.solutions){
+			System.out.println(solution.toString());
 		}
+		System.out.println("Failed consistency checks: "+tree.constraint_check_fail);
 	}
-	
+	 //CODE FOR QUESTION 2A
 	public static TreeNode constructNonHeuristicTree(){
-		LinkedList<TreeNode> frontier = new LinkedList<TreeNode>();
+		Stack<TreeNode> frontier = new Stack<TreeNode>();
 		TreeNode root = new TreeNode();
 		root.leaves = new ArrayList<>();
+		root.solutions = new ArrayList<>();
 		frontier.add(root);
 		while(frontier.size()>0){
-			TreeNode curr = frontier.remove();
+			TreeNode curr = frontier.pop();
 			if(!curr.hasAllAssigned()){
 				if(curr.consistencyCheck()){
 					for(int i=1; i<5; i++){
 						TreeNode child = curr.copy();
 						child.setNextVariable(i);
 						curr.addChild(child);
+						child.setParent(curr);
 						root.node_count++;
 						frontier.add(child);
 					}
@@ -36,10 +36,53 @@ public class main {
 			}
 			else{
 				root.leaves.add(curr);
+				if(curr.consistencyCheck()){
+					root.solutions.add(curr);
+				}
+			}
+			if(curr!=root){
+				curr.printElements();
+				if(!curr.consistencyCheck())
+					root.constraint_check_fail ++ ;
 			}
 		}
 		return root;
 	}
 	
+	//CODE FOR QUESTION 2B
+	public static TreeNodeHeuristicVersion constructHeuristicTree(){
+		Stack<TreeNodeHeuristicVersion> frontier = new Stack<>();
+		TreeNodeHeuristicVersion root = new TreeNodeHeuristicVersion();
+		root.leaves = new ArrayList<>();
+		root.solutions = new ArrayList<>();
+		frontier.add(root);
+		while(frontier.size()>0){
+			TreeNodeHeuristicVersion curr = frontier.pop();
+			if(!curr.hasAllAssigned()){
+				if(curr.consistencyCheck()){
+					for(int i=1; i<5; i++){
+						TreeNodeHeuristicVersion child = curr.copy();
+						child.setNextVariable(i);
+						curr.addChild(child);
+						child.setParent(curr);
+						root.node_count++;
+						frontier.add(child);
+					}
+				}
+			}
+			else{
+				root.leaves.add(curr);
+				if(curr.consistencyCheck()){
+					root.solutions.add(curr);
+				}
+			}
+			if(curr!=root){
+				curr.printElements();
+				if(!curr.consistencyCheck())
+					root.constraint_check_fail ++ ;
+			}
+		}
+		return root;
+	}
 	
 }
